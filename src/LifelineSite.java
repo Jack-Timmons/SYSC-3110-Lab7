956022afa16dd71b14d7f84ba49a83eeb4959ed5
jackt <jacktimmons@cmail.carleton.ca>
2025-11-07 14:59:47 -0500

    Added abstract method getTaxAmount() to Site

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -21,6 +21,10 @@ public class LifelineSite extends Site{
     }
 
     public double getBaseAmount() {
-        return _units*_rate;
+        return _units*_rate *0.5;
+    }
+
+    public double getTaxAmount(){
+        return getBaseAmount()*Site.TAX_RATE *0.2;
     }
 }
--- src/ResidentialSite.java
+++ src/ResidentialSite.java
@@ -23,4 +23,8 @@ public class ResidentialSite extends Site{
     public double getBaseAmount() {
         return _units*_rate;
     }
+
+    public double getTaxAmount(){
+        return getBaseAmount()*Site.TAX_RATE;
+    }
 }
